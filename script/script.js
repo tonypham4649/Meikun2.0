c6d37afa6d0e0890e6a3da137cf404887e0a93d1
Tony Pham <tonypham2vn@gmail.com>
2023-07-12 18:33:31 +0900

    newer

--- script/script.js
+++ script/script.js
@@ -166,4 +166,22 @@ function navForSP() {
     }
 }
 
-
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function navforMenu() {
+    document.getElementById("menu-dropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("sidebar-menu-nav");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
